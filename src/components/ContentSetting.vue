41afa03bdf3b382662b2ef2f71e7f8d77c85363a
darren <darrentu8@gmail.com>
2023-03-27 14:45:17 +0800

    add

--- src/components/ContentSetting.vue
+++ src/components/ContentSetting.vue
@@ -1,14 +1,74 @@
 <template>
-  <q-list>
-    <q-toolbar class="text-primary bg-grey-4">
-      <q-item-label header>
-        State 1
+  <div class="row">
+    <q-list class="col-12">
+      <q-item-label class="flex justify-between q-pa-sm items-center bg-grey-4">
+        <div class="col-5">
+          State 1
+        </div>
+        <div class="col-7">
+          Content settings
+        </div>
+        <q-btn color="grey" round flat dense :icon="expandedContent ? 'keyboard_arrow_up' : 'keyboard_arrow_down'"
+          @click="expandedContent = !expandedContent" />
       </q-item-label>
-      <q-item-label header>
-        Content settings
-      </q-item-label>
-    </q-toolbar>
-  </q-list>
+
+      <div class="col-5">
+        <q-slide-transition>
+          <div v-show="expandedContent" class="row">
+            <div class="col-5 column q-py-md mx-auto items-center">
+              <svg width="300" height="200" style="border: 1px solid; background: #fff;" viewbox="0,0,100,160"
+                preserveAspectRatio="xMidYMid slice">
+              </svg>
+            </div>
+            <div class="col-7 column q-gutter-md q-py-md" style="max-height: 300px;">
+              <q-select class="" filled dense v-model="contentData.media.source" :options="['local', 'Remote']"
+                label="Source" />
+              <q-select class="" filled dense v-model="contentData.media.viewType" :options="['local', 'Remote']"
+                label="View Type" />
+              <q-select class="" filled dense v-model="contentData.media.scaleType" :options="['local', 'Remote']"
+                label="Scale Type" />
+              <q-select class="" filled dense v-model="contentData.media.playEnd" :options="['local', 'Remote']"
+                label="Play End" />
+              <q-select class="" filled dense v-model="contentData.media.audio" :options="['local', 'Remote']"
+                label="Audio" />
+              <q-select class="" filled dense v-model="contentData.media.duration" :options="['local', 'Remote']"
+                label="Duration(Sec)" />
+              <q-toggle dense v-model="contentData.media.detectOrientation" label="Detect Orientation" />
+              <q-toggle dense v-model="contentData.media.repeatVideo" label="Repeat Video" />
+
+            </div>
+          </div>
+        </q-slide-transition>
+      </div>
+    </q-list>
+  </div>
 </template>
-<script setup>
+
+<script>
+import { defineComponent, ref } from 'vue'
+export default defineComponent({
+  name: 'ContentSetting',
+  components: {
+  },
+  setup() {
+    return {
+      expandedContent: ref(true),
+      contentData: {
+        media: {
+          source: '',
+          viewType: '',
+          fileUpload: '',
+          scaleType: '',
+          palyEnd: '',
+          audio: '',
+          duration: '',
+          detectOrientation: false,
+          repeatVideo: false
+        }
+      }
+
+    }
+  }
+
+})
 </script>
--- src/components/StoreFlow.vue
+++ src/components/StoreFlow.vue
@@ -1,11 +1,81 @@
 <template>
-  <q-list>
-    <q-toolbar class="text-primary bg-grey-4">
-      <q-item-label header>
-        Story Flow
+  <div class="row">
+    <q-list class="col-12">
+      <q-item-label class="flex justify-between q-pa-sm items-center bg-grey-4">
+        <div class="col-12">
+          Story Flow
+        </div>
+        <q-btn color="grey" round flat dense :icon="expandedFlow ? 'keyboard_arrow_up' : 'keyboard_arrow_down'"
+          @click="expandedFlow = !expandedFlow" />
       </q-item-label>
-    </q-toolbar>
-  </q-list>
+
+      <q-slide-transition class="col-12 bordered">
+        <div v-show="expandedFlow">
+          <q-card flat square bordered class="">
+            <div class="flowRow flex items-center q-py-md">
+              <div class="col-2 q-px-md">
+                <q-chip>Default</q-chip>
+              </div>
+              <div class="col-10">
+                <q-card flat square bordered class="flowBox">
+                  <q-img src="https://cdn.quasar.dev/img/parallax2.jpg">
+                  </q-img>
+                  <q-card-section class="q-pa-sm">
+                    <div class="text-subtitle2">State 1</div>
+                  </q-card-section>
+                </q-card>
+              </div>
+            </div>
+            <div flat square class="flowRow text-center q-pa-md">
+              <q-btn unelevated rounded color="grey-2" text-color="black" label="New flow" />
+            </div>
+          </q-card>
+
+        </div>
+      </q-slide-transition>
+    </q-list>
+  </div>
 </template>
-<script setup>
+
+<script>
+import { defineComponent, ref } from 'vue'
+export default defineComponent({
+  name: 'ContentSetting',
+  components: {
+  },
+  setup() {
+    return {
+      expandedFlow: ref(true),
+      contentData: {
+        media: {
+          source: '',
+          viewType: '',
+          fileUpload: '',
+          scaleType: '',
+          palyEnd: '',
+          audio: '',
+          duration: '',
+          detectOrientation: false,
+          repeatVideo: false
+        }
+      }
+
+    }
+  }
+
+})
 </script>
+<style lang="scss">
+.flowRow {
+  border-bottom: 1px dotted rgba(0, 0, 0, 0.12);
+
+  &:last-of-type {
+    border-top: 0 solid rgba(0, 0, 0, 0.12);
+    border-bottom: 1px solid rgba(0, 0, 0, 0.12);
+  }
+}
+
+.flowBox {
+  min-width: 150px;
+}
+</style>
