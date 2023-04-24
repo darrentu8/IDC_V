2c8d3e764721afaee6d91ca722ad6147d2bc3e8f
darren <darrentu8@gmail.com>
2023-04-24 17:39:44 +0800

    add file

--- src/components/SourceList.vue
+++ src/components/SourceList.vue
@@ -3,7 +3,7 @@
     <q-btn label="" v-on:click="selectFile()" flat class="theme-tab-btn" color="green" icon="add"
       style="height: 49.8px;margin-top: -16px;" />
     <q-file ref="fileRef" v-model="files" multiple style="display: none" v-bind:max-files="10" accept="image/*, video/*"
-      @input="startUpload()" />
+      @update:model-value="startUpload" />
   </q-item-label>
   <div class="q-mt-md">
     <div v-if="files.length">
@@ -13,7 +13,7 @@
         </div>
         <div class="col-9">
           <div class="full-width">
-            <div>{{ file.filename }}</div>
+            <div>{{ file.name }}</div>
             <!-- <q-file filled bottom-slots label="Pick File" counter v-model="files">
               <template v-slot:prepend>
                 <q-icon name="cloud_upload" @click.stop.prevent />
@@ -28,7 +28,7 @@
           </div>
         </div>
         <div class="col-2">
-          <q-btn label="" @click="delFile()" round flat class="" color="green" icon="delete_outline" />
+          <q-btn label="" @click="delFile(i)" round flat class="" color="green" icon="delete_outline" />
         </div>
       </div>
     </div>
@@ -51,13 +51,19 @@ export default defineComponent({
     selectFile() {
       this.$refs.fileRef.pickFiles()
     },
-    async startUpload(file) {
-      console.log('file', file)
+    delFile(i) {
+      this.files.splice(i, 1)
+    },
+    async startUpload() {
+      const uploadedFiles = Array.from(this.fileRef.files)
+      // 將每個檔案加入到 files 陣列中
+      uploadedFiles.forEach((file) => {
+        console.log('file', file)
+        this.files.push(file)
+      })
       try {
-        // if (file) {
-        //   this.files.push(file)
-        // }
-        for (const file of this.files) {
+        for (const file of uploadedFiles) {
+          console.log('file', file)
           const filePath = file.url // 或許需要根據實際情況修改這裡
           await window.myAPI?.copyFile(filePath)
         }
