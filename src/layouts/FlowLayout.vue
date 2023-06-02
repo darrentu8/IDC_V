5cac00dd1d5acf378ed2aaa4bbc75a0f26b55edd
darren <darrentu8@gmail.com>
2023-06-02 11:42:53 +0800

    add process

--- src/layouts/FlowLayout.vue
+++ src/layouts/FlowLayout.vue
@@ -141,12 +141,32 @@ async function preview() {
       const result = window.myAPI.storeToXML(widgetStore.NovoDS._Playlist_Name, widgetStore.nowPlayListFolder, widgetStore.nowPlayListPath, xmlData)
       if (result) {
         window.myAPI?.writeJson(widgetStore.NovoDS._Playlist_Name).then(() => {
-          $q.dialog({
-            title: 'Preview' + ' ' + widgetStore.NovoDS._Playlist_Name + ' ' + 'now !'
-          }).onOk(() => {
-          }).onCancel(() => {
-          }).onDismiss(() => {
+          const dialog = $q.dialog({
+            title: 'Preview' + ' ' + widgetStore.NovoDS._Playlist_Name + '...',
+            message: 'Processing... 0%',
+            progress: true, // we enable default settings
+            persistent: false, // we want the user to not be able to close it
+            ok: false // we want the user to not be able to close it
           })
+
+          // we simulate some progress here...
+          let percentage = 0
+          const interval = setInterval(() => {
+            percentage = Math.min(100, percentage + Math.floor(Math.random() * 22))
+
+            // we update the dialog
+            dialog.update({
+              message: `Processing... ${percentage}%`
+            })
+
+            // if we are done, we're gonna close it
+            if (percentage === 100) {
+              clearInterval(interval)
+              setTimeout(() => {
+                dialog.hide()
+              }, 350)
+            }
+          }, 100)
         })
       }
     }
